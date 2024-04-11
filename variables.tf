50fbb2b6b375c1a82870448ac6756124bbeb8caf
tacobayle <nicolas.nayle@gmail.com>
2024-04-11 22:47:14 +0300

    20240411_k8s_cluster

--- variables.tf
+++ variables.tf
@@ -1,21 +1,7 @@
 #
-# Environment Variables
-#
-variable "vsphere_username" {}
-variable "vsphere_password" {}
-variable "vsphere_server" {}
-variable "avi_password" {}
-variable "avi_old_password" {}
-variable "avi_username" {}
-variable "docker_registry_username" {}
-variable "docker_registry_password" {}
-variable "docker_registry_email" {}
-
-#
 # Other Variables
 #
 
-variable "avi" {}
 
 variable "vcenter" {
   type = map
@@ -50,36 +36,7 @@ variable "controller" {
   }
 }
 
-variable "jump" {
-  type = map
-  default = {
-    name = "jump"
-    cpu = 2
-    memory = 4096
-    disk = 20
-    public_key_path = "~/.ssh/cloudKey.pub"
-    private_key_path = "~/.ssh/cloudKey"
-    wait_for_guest_net_timeout = 2
-    template_name = "ubuntu-focal-20.04-cloudimg-template"
-    avisdkVersion = "22.1.5"
-    username = "ubuntu"
-  }
-}
 
-variable "ansible" {
-  default = {
-    aviPbAbsentUrl = "https://github.com/tacobayle/ansibleAviClear"
-    aviPbAbsentTag = "v1.04"
-    aviConfigureUrl = "https://github.com/tacobayle/ansibleAviConfig"
-    aviConfigureTag = "v1.76"
-    version = {
-      ansible = "5.7.1"
-      ansible-core = "2.12.5"
-    }
-    k8sInstallUrl = "https://github.com/tacobayle/ansibleK8sInstall"
-    k8sInstallTag = "v1.75"
-  }
-}
 
 variable "backend_vmw" {
   default = {
