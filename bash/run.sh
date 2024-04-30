a08a830a6aa6189caa2dd9079d57140d3dc12d10
tacobayle <nicolas.nayle@gmail.com>
2024-04-30 15:00:27 +0300

    20240430_us_lab

--- bash/run.sh
+++ bash/run.sh
@@ -4,3 +4,7 @@ sleep 5
 terraform destroy -auto-approve -var-file=avi.json
 cd ~
 rm -fr tfAviVcenterLscAko ; git clone https://github.com/tacobayle/tfAviVcenterLscAko ; cd tfAviVcenterLscAko ; terraform init ; terraform apply -auto-approve -var-file=avi.json
+
+# us
+cd ~
+rm -fr tfAviVcenterLscAko ; git clone https://github.com/tacobayle/tfAviVcenterLscAko ; cd tfAviVcenterLscAko ; cp variables.tf.us variables.tf ; terraform init ; terraform apply -auto-approve -var-file=avi.json
